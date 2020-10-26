75be5433efe66cb691567b78ec5c227d00cd69ba
seungWonU <ysw8160@naver.com>
2020-10-26 21:00:33 +0900

    new

---
+++ app/src/main/java/com/example/kakaoclone/FriendListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kakaoclone
+
+import android.graphics.drawable.Drawable
+
+class FriendListActivity {
+
+         val img: Drawable? = null
+         val name: String? = null
+         val message: String? = null
+         val song: String?= null
+
+}
--- app/src/main/java/com/example/kakaoclone/MainActivity.kt
+++ app/src/main/java/com/example/kakaoclone/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.kakaoclone
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.ListView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val listview:ListView
+        val adapter: FriendListViewAdapter
+
+        listview = findViewById<View>(R.id.listView_Shape) as ListView
+
+        listview.adapter)
+
+        adapter.addItem(
+            ContextCompat.getDrawable(this, R.drawable.instagram), "instagram", "this is instagram"
+        )
+
+
+        listview.onItemClickListener =
+            OnItemClickListener { parent, v, position, id ->
+                val item: ListViewItem = parent.getItemAtPosition(position) as ListViewItem
+                Toast.makeText(applicationContext, item.getTitle(), Toast.LENGTH_LONG).show()
+            }
     }
 }
